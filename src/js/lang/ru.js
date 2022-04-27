a295be5069ce84c675fb7ff999ba68f41a3d6793
Vadzim Antonau <vanetthor@gmail.com>
2022-04-27 19:19:29 +0400

    feat: add Keyboard class to create keyboard DOM

---
+++ src/js/lang/ru.js
@@ -0,0 +1,308 @@
+export default [
+  {
+    code: 'Backquote',
+    key: 'ё',
+    shift: '',
+  },
+  {
+    code: 'Backslash',
+    key: '\\',
+    shift: '/',
+  },
+  {
+    code: 'Backspace',
+    key: 'Backspace',
+    shift: null,
+  },
+  {
+    code: 'BracketLeft',
+    key: 'х',
+    shift: '',
+  },
+  {
+    code: 'BracketRight',
+    key: 'ъ',
+    shift: '',
+  },
+  {
+    code: 'Comma',
+    key: 'б',
+    shift: '',
+  },
+  {
+    code: 'Digit0',
+    key: '0',
+    shift: ')',
+  },
+  {
+    code: 'Digit1',
+    key: '1',
+    shift: '!',
+  },
+  {
+    code: 'Digit2',
+    key: '2',
+    shift: '"',
+  },
+  {
+    code: 'Digit3',
+    key: '3',
+    shift: '№',
+  },
+  {
+    code: 'Digit4',
+    key: '4',
+    shift: ';',
+  },
+  {
+    code: 'Digit5',
+    key: '5',
+    shift: '%',
+  },
+  {
+    code: 'Digit6',
+    key: '6',
+    shift: ':',
+  },
+  {
+    code: 'Digit7',
+    key: '7',
+    shift: '?',
+  },
+  {
+    code: 'Digit8',
+    key: '8',
+    shift: '*',
+  },
+  {
+    code: 'Digit9',
+    key: '9',
+    shift: '(',
+  },
+  {
+    code: 'Equal',
+    key: '=',
+    shift: '+',
+  },
+  {
+    code: 'KeyA',
+    key: 'ф',
+    shift: '',
+  },
+  {
+    code: 'KeyB',
+    key: 'и',
+    shift: '',
+  },
+  {
+    code: 'KeyC',
+    key: 'с',
+    shift: '',
+  },
+  {
+    code: 'KeyD',
+    key: 'в',
+    shift: '',
+  },
+  {
+    code: 'KeyE',
+    key: 'у',
+    shift: '',
+  },
+  {
+    code: 'KeyF',
+    key: 'а',
+    shift: '',
+  },
+  {
+    code: 'KeyG',
+    key: 'п',
+    shift: '',
+  },
+  {
+    code: 'KeyH',
+    key: 'р',
+    shift: '',
+  },
+  {
+    code: 'KeyI',
+    key: 'ш',
+    shift: '',
+  },
+  {
+    code: 'KeyJ',
+    key: 'о',
+    shift: '',
+  },
+  {
+    code: 'KeyK',
+    key: 'л',
+    shift: '',
+  },
+  {
+    code: 'KeyL',
+    key: 'д',
+    shift: '',
+  },
+  {
+    code: 'KeyM',
+    key: 'ь',
+    shift: '',
+  },
+  {
+    code: 'KeyN',
+    key: 'т',
+    shift: '',
+  },
+  {
+    code: 'KeyO',
+    key: 'щ',
+    shift: '',
+  },
+  {
+    code: 'KeyP',
+    key: 'з',
+    shift: '',
+  },
+  {
+    code: 'KeyQ',
+    key: 'й',
+    shift: '',
+  },
+  {
+    code: 'KeyR',
+    key: 'к',
+    shift: '',
+  },
+  {
+    code: 'KeyS',
+    key: 'ы',
+    shift: '',
+  },
+  {
+    code: 'KeyT',
+    key: 'е',
+    shift: '',
+  },
+  {
+    code: 'KeyU',
+    key: 'г',
+    shift: '',
+  },
+  {
+    code: 'KeyV',
+    key: 'м',
+    shift: '',
+  },
+  {
+    code: 'KeyW',
+    key: 'ц',
+    shift: '',
+  },
+  {
+    code: 'KeyX',
+    key: 'ч',
+    shift: '',
+  },
+  {
+    code: 'KeyY',
+    key: 'н',
+    shift: '',
+  },
+  {
+    code: 'KeyZ',
+    key: 'я',
+    shift: '',
+  },
+  {
+    code: 'Minus',
+    key: '-',
+    shift: '_',
+  },
+  {
+    code: 'Period',
+    key: 'ю',
+    shift: '',
+  },
+  {
+    code: 'Quote',
+    key: 'э',
+    shift: '',
+  },
+  {
+    code: 'Semicolon',
+    key: 'ж',
+    shift: '',
+  },
+  {
+    code: 'Slash',
+    key: '.',
+    shift: ',',
+  },
+  {
+    code: 'AltLeft',
+    key: 'Alt',
+    shift: null,
+  },
+  {
+    code: 'AltRight',
+    key: 'Alt',
+    shift: null,
+  },
+  {
+    code: 'CapsLock',
+    key: 'CapsLock',
+    shift: null,
+  },
+  {
+    code: 'ContextMenu',
+    key: 'ContextMenu',
+    shift: null,
+  },
+  {
+    code: 'ControlLeft',
+    key: 'Control',
+    shift: null,
+  },
+  {
+    code: 'ControlRight',
+    key: 'Control',
+    shift: null,
+  },
+  {
+    code: 'Enter',
+    class: 'blue',
+    key: 'Enter',
+    shift: null,
+  },
+  {
+    code: 'MetaLeft',
+    key: 'Meta',
+    shift: null,
+  },
+  {
+    code: 'ShiftLeft',
+    key: 'Shift',
+    shift: null,
+  },
+  {
+    code: 'ShiftRight',
+    key: 'Shift',
+    shift: null,
+  },
+  {
+    code: 'Space',
+    class: 'blue',
+    key: ' ',
+    shift: ' ',
+  },
+  {
+    code: 'Tab',
+    key: 'Tab',
+    shift: '',
+  },
+  { code: 'ArrowLeft', key: '←', shift: null },
+  { code: 'ArrowUp', key: '↑', shift: null },
+  { code: 'ArrowDown', key: '↓', shift: null },
+  { code: 'ArrowRight', key: '→', shift: null },
+];
